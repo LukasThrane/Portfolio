88a325f2f6cd8a89893137336432078c7779e8d7
Lukas <lukas@thrane.name>
2024-09-05 02:19:05 +0200

    vercel fix

--- portfolio/app/projects/page.tsx
+++ portfolio/app/projects/page.tsx
@@ -2,7 +2,7 @@ export default function Projects() {
   return (
     <>
       <h1>Projects</h1>
-      <p>Here are some of the projects I've worked on.</p>
+      <p>Here are some of the projects I have worked on.</p>
     </>
   );
 }
